3ce60a38c1ef5f100a0fb0bfee7c1fe32bd14a82
DavidLujan10 <JoseDavid.LujanGallegos@hotmail.com>
2017-09-22 22:26:17 -0500

    Tareas

---
+++ Pract19/js/scripts.js
@@ -0,0 +1,111 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getvideo(){
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+    .then(localMediaStream => {
+        console.log(localMediaStream);
+        video.src = window.URL.createObjectURL(localMediaStream);
+        video.play();
+    })
+    .cath(err => {
+        console.error(`ERROR!!`, err);
+    });
+}
+
+function paintToCanvas(){
+    const width = video.videoWidth;//LA CONSTANTE QUE NOS DA EL ANCHO  DEL CANVAS DEL VIDEO
+    const height = video.videoHeigth;//LA CONSTANTE QUE NOS DA EL ALTO DEL CANVAS DEL VIDEO
+    canvas.width = width;//ANCHO  DEL CANVAS DEL VIDEO
+    canvas.height = height;//ALTO DEL CANVAS DEL VIDEO
+    
+    return setInterval(() =>{
+        ctx.drawImage (video, 0, 0, width, height);
+        //SACAMOS LOS PIXELES
+        const pixels = ctx.getImageData(0, 0, width, height);
+        //console.log(pixels);
+        //ES UN LIO AQUI CON ELLOS
+        //pixels = redEffect(pixels);
+
+        //pixels = rgbSplit(pixels);
+        //ctx.globalAlpha = 0.1; //La propiedad globalAlpha establece o devuelve el valor alfa o de transparencia actual del dibujo.
+
+        pixels = greenScreen(pixels);
+
+        //LOS DEVOLVEMOS
+        //debugger;
+        ctx.putImage = ctx.getImageData(0, 0, width, height);
+
+    }, 16);
+}
+
+function takePhoto(){
+    //TOCABAN LOS SONIDOS
+    snap.currentTime = e;
+    snap.play();
+
+    //SACAMOS LOS DATOS DEL CANVAS
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.textContent = 'Download Image';
+    link.innerHTML = `<ing scr = "${data}" alt="Handsome Man" />`;
+    strip.insertBefore(link,strip.firstChild);
+
+}
+
+function redEffect(pixels){
+    for(let i = 0; i < pixels.data.length; i+=4){
+        pixels.data[i + 0] = pixels.data[i + 0] + 100; //ROJO
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; //VERDE
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //BLUE
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels){
+    for(let i = 0; i < pixels.data.length; i+=4){
+        pixels.data[i - 150] = pixels.data[i + 0]; //ROJO
+        pixels.data[i + 500] = pixels.data[i + 1]; //VERDE
+        pixels.data[i - 550] = pixels.data[i + 2]; //BLUE
+    }
+    return pixels;
+}
+
+function greenScreen(pixels){
+    const levels = {};
+    
+      document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+      });
+
+      //console.log(levels);
+    
+      for (i = 0; i < pixels.data.length; i = i + 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+    
+        if (red >= levels.rmin
+          && green >= levels.gmin
+          && blue >= levels.bmin
+          && red <= levels.rmax
+          && green <= levels.gmax
+          && blue <= levels.bmax) {
+          // take it out!
+          pixels.data[i + 3] = 0;
+        }
+      }
+    
+      return pixels;
+}
+
+getvideo();
+
+video.addEventListener('canplay', paintToCanvas);
+
